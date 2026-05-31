6148a7a99c6de9b6d4466f53a024b06c9e016b69
kboddu <krantib@nfinity-global.com>
2026-05-31 12:39:58 +0530

    Element selection

--- app/_src/Context/GetFrameDetails.tsx
+++ app/_src/Context/GetFrameDetails.tsx
@@ -3,19 +3,29 @@ import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useStat
 
 interface GenerateWebsiteStates {
     frameData: any,
-    setFrameData: Dispatch<SetStateAction<any>>
+    setFrameData: Dispatch<SetStateAction<any>>,
+    selectedEl: any,
+    setSelectedEl: Dispatch<SetStateAction<any>>
 }
 
 
 export const GetFrameDetailscontext = createContext<GenerateWebsiteStates>({
     frameData: {},
-    setFrameData: () => { }
+    setFrameData: () => { },
+    selectedEl: {},
+    setSelectedEl: () => { }
 })
 
 export const GetFrameDetailsProvider = ({ children }: { children: ReactNode }) => {
     const [frameData, setFrameData] = useState<any>()
+    const [selectedEl, setSelectedEl] = useState<HTMLElement>()
     return (
-        <GetFrameDetailscontext.Provider value={{ frameData, setFrameData }}>
+        <GetFrameDetailscontext.Provider value={{
+            frameData,
+            setFrameData,
+            selectedEl,
+            setSelectedEl
+        }}>
             {children}
         </GetFrameDetailscontext.Provider>
     )
